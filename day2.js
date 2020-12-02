134386bd81bde1ffcef94f9a6bb90748c5fc23cb
Rene Pot <rene@topener.nl>
2020-12-02 15:24:28 +0000

    add day 2 part 2

--- day2.js
+++ day2.js
@@ -13,7 +13,7 @@ function split(item) {
 function isValidPart1(item) {
     const parts = split(item);
     if (!parts) return;
-    
+
     const char = parts[1].substr(0,1);
     const regex = new RegExp(char, 'g');
     const matches = parts[2].match(regex);
@@ -25,16 +25,39 @@ function isValidPart1(item) {
     }
 }
 
+
+function isValidPart2(item) {
+    const parts = split(item);
+    if (!parts) return;
+    
+    const char = parts[1].substr(0,1);
+    const pw = parts[2];
+
+    const char1 = pw.substr(parts[0][0] - 1, 1);
+    const char2 = pw.substr(parts[0][1] - 1, 1);
+
+    if (char1 === char2) return false;
+    if (char1 === char || char2 === char) return true;
+
+    return false;
+
+}
+
 function parseItems(input) {
     let validItems1 = 0;
+    let validItems2 = 0;
     
     input.forEach(item => {
         if (isValidPart1(item)) {
             validItems1++;
         }
+        if (isValidPart2(item)){
+            validItems2++;
+        }
     });
 
     console.log('part 1: found valid items: ', validItems1);
+    console.log('part 2: found valid items: ', validItems2);
 }
 
 
