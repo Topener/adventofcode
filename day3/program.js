ab872f549a03842bc8e3f45d96bd493442526483
Rene Pot <rene@topener.nl>
2020-12-03 11:47:39 +0000

    day 3 part 2

--- day3/program.js
+++ day3/program.js
@@ -3,22 +3,33 @@ let items;
 
 fs.readFile('./input.txt', 'utf-8', (e, data) => {
     items = data.split("\n");
-    const result = progress(0, 0, 0);
-    console.log('result!', result);
+    const resultPart1 = progress(0, 0, 0, [3,1]);
+    const resultPart2 = checkPart2();
+
+    console.log('result!', resultPart1, resultPart2);
 });
 
-function progress(trees, currentRow, currentColumn) {
+function checkPart2() {
+    const oneone = progress(0, 0, 0, [1,1]);
+    const threeone = progress(0, 0, 0, [3,1]);
+    const fiveone = progress(0, 0, 0, [5,1]);
+    const sevenone = progress(0, 0, 0, [7,1]);
+    const onetwo = progress(0, 0, 0, [1,2]);
+    return oneone*threeone*fiveone*sevenone*onetwo;
+}
+
+function progress(trees, currentRow, currentColumn, nextStep) {
     const block = items[currentRow].substr(currentColumn, 1);
     if (block === '#') trees++;
 
-    currentRow++;
+    currentRow += nextStep[1];
     if (!items[currentRow]) return trees;
     
-    currentColumn +=3;
+    currentColumn += nextStep[0];
 
     if (currentColumn > items[currentRow].length - 1) {
         currentColumn -= items[currentRow].length;
     }
     
-    return progress(trees, currentRow, currentColumn);
+    return progress(trees, currentRow, currentColumn, nextStep);
 }
