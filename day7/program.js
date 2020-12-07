a231080751cd9551d16d8079205af9ce6d23029a
Rene Pot <rene@topener.nl>
2020-12-07 11:15:48 +0000

    add part 2

--- day7/program.js
+++ day7/program.js
@@ -14,7 +14,8 @@ for (color in allBags) {
 
 console.log('bags that eventually contain a shiny gold bag', matches);
 
-
+const nestedBags = howManyNestedBags('shiny gold');
+console.log(`We got ${nestedBags} bags nested in shiny gold`);
 
 function extractRules(rules) {
     let bagtypes = {};
@@ -28,6 +29,7 @@ function extractRules(rules) {
         nestedBags.forEach(bag => {
             let count = bag.substr(0,bag.indexOf(' '));
             if (count === 'no') count = 0;
+            count = parseInt(count);
             const bagType = bag.substr(bag.indexOf(' ')+1);
             let subBagType = bagType.replace(' bags', '').replace(' bag', '');
             bagtypes[parts[0]][subBagType] = count;
@@ -52,4 +54,22 @@ function willContainColor(bags, color, matched) {
         }
         
     }
+}
+
+function howManyNestedBags(color) {
+    let bagCount = 0;
+
+    for (subColor in allBags[color]) {
+        if (subColor !== 'other') {
+            bagCount += allBags[color][subColor];
+            const subColorCount = allBags[color][subColor];
+            let subCount = howManyNestedBags(subColor);
+            if (subCount > 0) {
+                
+                subCount = subCount * subColorCount;
+                bagCount += subCount;
+            }
+        }
+    }
+    return bagCount;
 }
