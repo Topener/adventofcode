6fe2c60f7b88e7cf0a82133c4fa8b01a83c3e6fd
Rene Pot <rene@topener.nl>
2021-12-01 19:39:37 +0100

    added central loader

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-import loadDay from "./helper.js";
+import loadData, { fileExists } from "./helper.js";
 
 if (process.argv[2]) {
-    const dayName = `day${process.argv[2]}`;
-    const data = loadDay(dayName);
-    const { default: Day } = await import(`./${dayName}/program.js`);
-    Day(data);
+    try {
+        const dayName = `day${process.argv[2]}`;
+        const filename = `./${dayName}/program.js`;
+
+        if (await fileExists(filename)) {
+            console.log("found", filename);
+            const data = await loadData(dayName);
+            const { default: Day } = await import(filename);
+
+            console.time("Day Execution");
+            Day(data);
+            console.timeEnd("Day Execution");
+        } else {
+            console.log("The provided day does not exist or is not yet implemented");
+        }
+    } catch (e) {
+        console.log(e);
+        error();
+    }
 } else {
-    console.log("Please specify a day as a number");
+    error();
     process.exit(1);
+}
+
+function error() {
+    console.log('Please specify a day as a number like "node index.js 1"');
 }
