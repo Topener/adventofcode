21960a2eadc4e4b5663fc70593148d8dcf3d1208
Rene Pot <rene@topener.nl>
2021-12-01 19:39:37 +0100

    added central loader

--- helper.js
+++ helper.js
@@ -4,3 +4,12 @@ export default async function loadData(day) {
     const data = await fs.readFileSync(`./${day}/input.txt`, "utf-8");
     return data.split("\n");
 }
+
+export async function fileExists(filename) {
+    try {
+        const result = await fs.existsSync(filename);
+        return result;
+    } catch (err) {
+        return false;
+    }
+}
