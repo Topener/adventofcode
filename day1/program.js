198146603bda5b85d7a1c5815460f2e3c0ca7184
Rene Pot <rene@topener.nl>
2021-12-01 19:39:44 +0100

    day 1

--- day1/program.js
+++ day1/program.js
@@ -1,3 +1,44 @@
 export default function Day(data) {
-    console.log(data);
+    let increases = 0;
+    let lastValue = false;
+
+    // prepare data
+    data.forEach((row, i) => {
+        data[i] = parseInt(row);
+    });
+
+    // part 1
+    data.forEach((row) => {
+        if (lastValue !== false && row > lastValue) {
+            increases++;
+        }
+
+        lastValue = row;
+    });
+
+    console.log("Part 1:", increases);
+
+    // part 2
+    let rollingThrees = [];
+    let lastSum = false;
+    let increases2 = 0;
+
+    data.forEach((row, i) => {
+        rollingThrees.push(row);
+
+        if (rollingThrees.length > 3) {
+            rollingThrees.shift();
+        }
+        if (rollingThrees.length === 3) {
+            const sum = rollingThrees.reduce((a, b) => a + b);
+
+            if (lastSum !== false && sum > lastSum) {
+                increases2++;
+            }
+
+            lastSum = sum;
+        }
+    });
+
+    console.log("Part 2:", increases2);
 }
