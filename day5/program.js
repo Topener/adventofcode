7fddfa9768edbe7a5cce3e1cfc7f8b9a0865ac19
Rene Pot <rene@topener.nl>
2022-12-05 09:55:23 +0100

    remove debug

--- day5/program.js
+++ day5/program.js
@@ -12,7 +12,7 @@ function extractStacks(data) {
     }
 
     const containerRows = [];
-    // use a while loop to loop through data until a row has a 1 on position 2 in the string
+
     let i = 0;
     while (i < data.length) {
         if (data[i][1] === '1') {
@@ -28,10 +28,8 @@ function extractStacks(data) {
         parts.forEach((part, i) => {
             if (part === '') return pos++;
             if (part.substr(0,1) === '[') pos += 4;
-            console.log(pos, part);
             const stackNum = pos/4-1;
             stacks[stackNum].unshift(part);
         });
     });
-    console.log(stacks);
 }
