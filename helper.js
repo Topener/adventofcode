5d029fabc89173fe302c0c2881d049beca069e7f
Rene Pot <rene@topener.nl>
2023-12-01 22:12:06 +0100

    bootstrapping

--- helper.js
+++ helper.js
@@ -1,16 +1,37 @@
-import fs from "fs";
+import { readFile, access } from "fs/promises";
 
-export async function loadData(day) {
-    const filename = process.argv[3] ? "sample.txt" : "input.txt";
-    const data = await fs.readFile(`./${day}/${filename}`, "utf-8");
+export async function loadData({ day, dataType = "sample" }) {
+    const data = await readFile(`./${day}/${dataType}.txt`, "utf-8");
     return data.split("\n");
 }
 
 export async function fileExists(filename) {
     try {
-        const result = await fs.readFile(filename);
-        return result;
+        await access(filename);
+        return true;
     } catch (err) {
         return false;
     }
+}
+
+export async function argsChecker(args) {
+    if (args.length === 0) {
+        console.log("Please provide a day number");
+        process.exit(1);
+    }
+
+    let dataType = "sample";
+    if (args[1] === "input") {
+        dataType = "input";
+    }
+
+    return { dataType };
+}
+
+export async function partRunner({ part, input }) {
+    console.time(part.name);
+    const result = part(input);
+    console.timeEnd(part.name);
+    console.log(result);
+    return result;
 }
