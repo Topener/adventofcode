6f4bb54db0f78fc2bf395aef722d954a4eab488a
Rene Pot <rene@topener.nl>
2023-12-01 21:47:53 +0100

    prepare AoC 2023

--- empty/program.js
+++ empty/program.js
@@ -1,3 +1,9 @@
-export default function Day(data) {
-    
-}
+export const part1 = ({ input }) => {
+    const result = 0;
+    return result;
+};
+
+export const part2 = ({ input }) => {
+    const result = 0;
+    return result;
+};
--- helper.js
+++ helper.js
@@ -1,14 +1,14 @@
 import fs from "fs";
 
-export default async function loadData(day) {
-    const filename = process.argv[3] ? 'sample.txt' : 'input.txt';
-    const data = await fs.readFileSync(`./${day}/${filename}`, "utf-8");
+export async function loadData(day) {
+    const filename = process.argv[3] ? "sample.txt" : "input.txt";
+    const data = await fs.readFile(`./${day}/${filename}`, "utf-8");
     return data.split("\n");
 }
 
 export async function fileExists(filename) {
     try {
-        const result = await fs.existsSync(filename);
+        const result = await fs.readFile(filename);
         return result;
     } catch (err) {
         return false;
