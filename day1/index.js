e7e6cefec72ca841c178824ff7d1ac520df8870a
Rene Pot <rene@topener.nl>
2025-12-01 22:48:41 +0100

    prepare 2025

--- day1/index.js
+++ day1/index.js
@@ -1,41 +1,9 @@
 export const part1 = ({ input }) => {
     let result = 0;
-
-    const { list1, list2 } = parseInto2lists(input);
-    list1.sort();
-    list2.sort();
-
-    list1.forEach((item, index) => {
-        result += Math.abs(item - list2[index]);
-    });
-
     return result;
 };
 
 export const part2 = ({ input }) => {
     let result = 0;
-
-    const { list1, list2 } = parseInto2lists(input);
-
-    list1.forEach((item) => {
-        const count = list2.filter((matchItem) => matchItem == item).length;
-        if (count > 0) {
-            result += item * count;
-        }
-    });
-
     return result;
-};
-
-const parseInto2lists = (input) => {
-    const list1 = [];
-    const list2 = [];
-
-    input.forEach((item) => {
-        const split = item.split("   ");
-        list1.push(parseInt(split[0]));
-        list2.push(parseInt(split[1]));
-    });
-
-    return { list1, list2 };
 };
